9e9b1c32a4d4d13d732e68809b3a8e9a7527a4f4
Jacquelyn Moreno <jemoreno802@gmail.com>
2020-04-09 19:29:15 -0400

    DB integration for authentication

--- routes/login.js
+++ routes/login.js
@@ -4,20 +4,36 @@ var router = express.Router();
 router.get('/', function(req, res) {
     if(typeof sess !== "undefined"){
         if(typeof sess.user !== "undefined"){
-            sess.user = undefined; //Logout
+            sess = undefined; //logout
             res.render('index', {user: ""});
         }  
     }else{
-        res.render('login');
+        res.render('login', {message: ""});
     }
     
 });
 
-router.get('/loginClick', function(req,res){
-    sess = req.session;
-    console.log("sessionID: " + req.sessionID);
-    sess.user = 'Jackie'
-    res.render('index', {user: 'JACKIE'});
+router.post('/', function(req,res) {
+    let query = "SELECT * FROM openheart_db.users WHERE username = '" + req.body.username + "' AND pass = '" + req.body.password + "';";
+
+    db.query(query, (err, result) =>{
+        if(err){
+            console.log("DB ERROR");
+            res.redirect('/');
+        } else{
+            if(result.length){
+                console.log("authenticated");
+                sess = req.session;
+                sess.user = req.body.username;
+                console.log("username: " + req.body.username + " password: " + req.body.password);
+                res.render('index', {user: req.body.username});
+            } else{
+                console.log("incorrect credentials");
+                res.render('login', {message: "incorrect credentials"})
+            }
+            
+        }
+    });
 });
 
 module.exports = router;
